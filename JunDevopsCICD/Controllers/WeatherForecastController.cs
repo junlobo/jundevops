863559582e73762f2ff9136fb65796b288e98078
Jun Lobo <mr.junlobo@gmail.com>
2020-05-26 12:09:58 +1000

    deployment trigger

--- JunDevopsCICD/Controllers/WeatherForecastController.cs
+++ JunDevopsCICD/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace JunDevopsCICD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing22", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing33", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
